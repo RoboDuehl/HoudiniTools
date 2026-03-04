be374709c1eaa8a87a1396d0cb05aa6a8d51be95
Andreas <andal@MilPool.local>
2026-03-04 08:49:48 -0500

    env variable, packaged and cleaned

---
+++ create_package_json.py
@@ -0,0 +1,20 @@
+import json 
+import sys 
+import os 
+
+
+with open("user_data.json", "r") as file: 
+    user_data = json.load(file) 
+
+user_machine = os.getlogin()
+print(user_machine)
+json_dict = {} 
+
+with open("packages/company_vars.json", "w") as env_file: 
+    for user in user_data:
+        if user_machine in user_data[user].values(): 
+            print("User found, setting env varialbles") 
+            print(user_data[user])
+            json_dict["env"] = [{"USER_ID":str(user_data[user]["id"])}]
+            json.dump(json_dict, env_file, indent=4)
+            # check houdini documetation about the syntax of the packges      
