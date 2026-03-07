abbeb2056a774fa119765e518591f0d368a7da00
awieland-can <andall@gmx.net>
2026-03-07 16:29:59 -0500

    first tool

--- mainInterface.py
+++ mainInterface.py
@@ -1,9 +1,12 @@
 from tkinter import * 
 import zipfile 
+from tkinter import filedialog  
 import os 
 import sys 
 import glob 
 import pandas as pd 
+from unzipUtils import UnzipUtils   
+
 
 THEME_COLOR = "#444953"
 
@@ -23,14 +26,62 @@ class FolderInterface:
         self.open_btn_image = PhotoImage(file=r"buttons\open.png")
         self.openDir_btn = Button(bg=THEME_COLOR, image=self.open_btn_image, bd=0, highlightbackground=THEME_COLOR, highlightthickness=0, command=self.openDirectory)
         self.openDir_btn.grid(row=1, column=1)
+
+
+        #Central canvas 
+        self.central_canvas = Canvas(width= 200, height=200, highlightthickness=0, bg="white")
+        self.central_text = self.central_canvas.create_text(150, 75, width=200, text="", fill="#000000", font=("Arial", 20, "italic"))
+        self.central_canvas.grid(row=2, column=0, columnspan=3, pady=50)
+        
+        #TODO COMMAND BUTTONS 
+        #unizip button
+        self.unzip_btn_image = PhotoImage(file=r"buttons\unzip.png")
+        self.unzip_btn = Button(bg=THEME_COLOR, image=self.unzip_btn_image, bd=0, highlightbackground=THEME_COLOR, highlightthickness=0, command=self.unzipAllBtn)
+        self.unzip_btn.grid(row=5, column=1)    
+        
+        #csv exporter
+        self.csv_btn_image = PhotoImage(file=r"buttons\csv.png")
+        self.csv_btn = Button(bg=THEME_COLOR, image=self.csv_btn_image, bd=0, highlightbackground=THEME_COLOR, highlightthickness=0, command=self.exportCsv)
+        self.csv_btn.grid(row=6, column=1)    
+        
+        
+        #delete button
+        self.delete_btn_image = PhotoImage(file=r"buttons\delete.png")
+        self.delete_btn = Button(bg=THEME_COLOR, image=self.delete_btn_image, bd=0, highlightbackground=THEME_COLOR, highlightthickness=0, command=self.deleteZipFiles)
+        self.delete_btn.grid(row=7, column=1)    
+        
         self.window.mainloop()
 
-
-        #open dir function 
+    #open dir function 
     def openDirectory(self):
-        print("BUTTON working") 
+        self.opened_dir= filedialog.askdirectory() 
+        self.zip_files = [file for file in os.listdir(self.opened_dir) if file.endswith(".zip")]
+        self.central_canvas.itemconfig(self.central_text, text=f"{len(self.zip_files)} zip files found in: {self}")
 
+    def unzipAllBtn(self):
+        zip1 = UnzipUtils(self.opened_dir)
+        zip1.unzipAll()
 
-if __name__ =="__main__":
+    def deleteZipFiles(self):
+        for zfile in self.zip_files: 
+            os.remove(self.opened_dir + "/" + zfile)
+            print("done removing zip files")
+
+    def exportCsv(self):
+        dirpath = self.opened_dir
+        fbx_files = []
+
+        for x in os.walk(dirpath):
+            for y in glob.glob(os.path.join(x[0], "*.fbx")):
+                fbx_files.append(y)
+
+            h = {"fbx_files": fbx_files}
+            df = pd.DataFrame(h)
+            df.to_csv("file_list.csv")        
+
+            data = pd.read_csv("file_list.csv")
+            print(data)
+
+if __name__ == "__main__":
     ui = FolderInterface()
         
--- unzipUtils.py
+++ unzipUtils.py
@@ -14,6 +14,7 @@ class UnzipUtils:
             with zipfile.ZipFile(self.dirpath + "/" + zipf, "r") as zip_ref: 
                 zip_ref.extractall(self.dirpath)
             print(f"Done extracting {zipf}")
+            
         print("All files unzipped") 
 
     def deleteZipFiles(self): 
